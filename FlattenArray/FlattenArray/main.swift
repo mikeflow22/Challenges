78f373151c36ddd1603f56fb9ac833ed00515c4e
Michael <mrflowers22@uwalumni.com>
2020-12-15 05:46:53 -0800

    Solved it by using flatmap and chained it with sorted

--- FlattenArray/FlattenArray/main.swift
+++ FlattenArray/FlattenArray/main.swift
@@ -17,3 +17,12 @@ print("Hello, World!")
  Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
  */
 
+func twoDemensional(array: [[Int]]) -> [Int] {
+    //On first thought I'm thinking that there is a collection's method that will flatten the two dimensional array
+    //and then we can sort the single array
+    
+   
+    return array.flatMap({$0}).sorted()
+}
+
+print(twoDemensional(array: [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]]))
