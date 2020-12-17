5d7ea6cfa15857989ca9729e7dabda04fda23906
Michael <mrflowers22@uwalumni.com>
2020-12-17 08:10:15 -0800

    second attempt with a switch statement failed.

--- NumberInExpandedForm/NumberInExpandedForm/main.swift
+++ NumberInExpandedForm/NumberInExpandedForm/main.swift
@@ -10,9 +10,9 @@ import Foundation
 /*
  CREDIT TO "CODEWARS"
  Write Number in Expanded Form
-
+ 
  You will be given a number and you will need to return it as a string in Expanded Form. For example:
-
+ 
  expandedForm 12    -- Should return '10 + 2'
  expandedForm 42    -- Should return '40 + 2'
  expandedForm 70304 -- Should return '70000 + 300 + 4'
@@ -27,12 +27,24 @@ func expandedForm(input: Int) -> String {
     //tens
     //hundreds
     //thousands
+    var thousands: Int = 0
+    var output: String = ""
     
-    let ten: Int? = input / 10
-    let remainder = input % 10
-    print(remainder)
-    print(ten)
-    return "something"
+    //this approach will turn the input into a string, so that we can get the "digits" this will give us information on how big the number is and from there we can disect the input
+    
+    let digits = String(input)
+    
+    switch digits.count {
+    case 5:
+        thousands = input / 10000
+        output += "\(thousands)"
+    default:
+        print("something happened")
+    
+    }
+    
+    
+    return output
     
 }
-print(expandedForm(input: 28))
+print(expandedForm(input: 70000))
