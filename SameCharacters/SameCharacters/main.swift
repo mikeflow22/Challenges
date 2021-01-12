0800f4dc950ecbfaa14e5a4431bb3f958ae38224
Michael <mrflowers22@uwalumni.com>
2021-01-11 21:14:19 -0800

    copied and pasted challenge from PH's book into command line

--- SameCharacters/SameCharacters/main.swift
+++ SameCharacters/SameCharacters/main.swift
@@ -6,6 +6,19 @@
 //
 
 import Foundation
+/*
+ “Write a function that accepts two String parameters, and returns true if they contain the same characters in any order taking into account letter case.
 
-print("Hello, World!")
+ Sample input and output
+ The strings “abca” and “abca” should return true.
+ The strings “abc” and “cba” should return true.
+ The strings “ a1 b2 ” and “ b1 a2 ” should return true.
+ The strings “abc” and “abca” should return false.
+ The strings “abc” and “Abc” should return false.
+ The strings “abc” and “cbAa” should return false.
+ The strings “abcc” and “abca” should return false.
+ ”
+
+ Excerpt From: Paul Hudson. “Swift Coding Challenges.” Apple Books.
+ */
 
