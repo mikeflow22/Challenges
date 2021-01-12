cb98fb785f6f637a15073df54a2837a878ca91c1
Michael <mrflowers22@uwalumni.com>
2021-01-12 12:26:16 -0800

    copied and pasted Challenge from PH's book

--- ContainAnotherString/ContainAnotherString/main.swift
+++ ContainAnotherString/ContainAnotherString/main.swift
@@ -7,5 +7,14 @@
 
 import Foundation
 
-print("Hello, World!")
+/*
+ “Write your own version of the contains() method on String that ignores letter case, and without using the existing contains() method.
+
+ Sample input and output
+ The code "Hello, world".fuzzyContains("Hello") should return true.
+ The code "Hello, world".fuzzyContains("WORLD") should return true.
+ The code "Hello, world".fuzzyContains("Goodbye") should return false.”
+
+ Excerpt From: Paul Hudson. “Swift Coding Challenges.” Apple Books.
+ */
 
