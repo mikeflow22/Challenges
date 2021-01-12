9b07e345e08c9386688c8989cc45a47c01ec5ab4
Michael <mrflowers22@uwalumni.com>
2021-01-11 21:27:41 -0800

    first attempt at using a Set failed

--- SameCharacters/SameCharacters/main.swift
+++ SameCharacters/SameCharacters/main.swift
@@ -22,3 +22,20 @@ import Foundation
  Excerpt From: Paul Hudson. “Swift Coding Challenges.” Apple Books.
  */
 
+func sameCharacters(input1: String, input2: String) -> Bool {
+    //first I want to check to see if the strings match in length
+    guard input1.count == input2.count else {
+        return false
+    }
+    
+    //Next I want to check to see if each string contains the same characters, I can do this with my own algorithm.
+    
+    let set1 = Set(input1)
+    print(set1)
+    let set2 = Set(input2)
+    print(set2)
+    return set1.intersection(set2).count == 0
+}
+
+print(sameCharacters(input1: "abca", input2: "abca"))
+print(sameCharacters(input1: "abc", input2: "abca"))
